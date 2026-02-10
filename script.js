e3d60a0c2f3bbcbcd8377dff3cc6c0ea04b20b76
wojtaz45-cell <wojtaz45@gmail.com>
2026-02-10 10:15:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+const music = document.getElementById("music");
+
+// uciekający przycisk NIE
+function moveButton() {
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 100;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+noBtn.addEventListener("mouseenter", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
+// kliknięcie TAK
 yesBtn.addEventListener("click", () => {
     // start muzyki – bezpiecznie
     music.volume = 0.6;
@@ -10,21 +25,19 @@ yesBtn.addEventListener("click", () => {
     document.querySelector(".card").innerHTML = `
         <h1>Yaaaay! 💕</h1>
         <p class="question">
-            Ale ma...Ez 🌹🌹  
+            Naprawdę bardzo się cieszę 🥹  
             <br><br>
-            Kocham Cię Robaczku.  
-            Jesteś moim całym światem.  
-            Buziaczki” 💖💖
+            Dziękuję Ci, Natalko.  
+            Obiecuję dużo uśmiechów, ciepłych chwil  
+            i jeszcze więcej powodów do „aww” 💖
         </p>
         <img class="gif"
-             src="https://media.tenor.com/-FI5WRxJ23kAAAAi/peach-goma-love.gif">
+             src="https://tenor.com/pl/view/happy-valentines-day-love-happy-valentines-day-my-love-i-love-you-love-happy-valentines-day-gif-7872935811532978148">
     `;
 });
 
-
     initConfetti();
 });
-
 
 
 
